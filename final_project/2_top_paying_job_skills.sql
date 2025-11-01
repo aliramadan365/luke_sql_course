bf6498394e1f4be6e9e4347b46d95f7feb7660ff
Ali <aliramadan365@gmail.com>
2025-11-01 17:49:50 +0200

    deleting some comments

--- final_project/2_top_paying_job_skills.sql
+++ final_project/2_top_paying_job_skills.sql
@@ -1,31 +1,3 @@
-/* select 
-    j.job_id,
-    job_title,
-    job_location,
-    c.name as company_name,
-    s.skills as required_skills,
-    c.link,
-    c.link_google,
-    job_schedule_type,
-    
-    to_char (round(salary_year_avg,0),'9,999,999') as salary_year_avg,
-    job_posted_date
-from job_postings_fact j
-left join company_dim c on j.company_id = c.company_id
-left join skills_job_dim sj on j.job_id = sj.job_id
-LEFT JOIN skills_dim s on sj.skill_id = s.skill_id
-where   job_title_short = 'Data Analyst' AND 
-        salary_year_avg is not NULL AND
-        job_location = 'Anywhere'
-order by salary_year_avg desc
-limit 100;
-
-
-select * from skills_dim
-limit 1
-
-select * from skills_job_dim
-limit 1 */
 
 with top_paying_jobs AS (
         select 
